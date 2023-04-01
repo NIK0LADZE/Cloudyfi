6c9749360d64319c2c7b948460288c3aafa3760b
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2023-04-01 15:15:17 +0400

    Project init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './css/tailwind.css'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

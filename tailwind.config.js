40f85034ee7f92c1ed02e46266c5c232ea974b15
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2023-04-01 23:48:31 +0400

    Cloudyfi description block was added and styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,27 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundColor: {
-        'gray': '#EBEBEB'
+      colors: {
+        customGray: {
+          100: '#EBEBEB',
+          200: '#F4F4F4',
+          900: '#515151'
+        }
       },
       backgroundImage: {
-        'logo': 'url(../svg/logo.svg)',
-        'light-blue-shape': 'url(../svg/light-blue-shape.svg)',
-        'purple-shape': 'url(../svg/purple-shape.svg)',
-        'blue-shape': 'url(../svg/blue-shape.svg)',
+        'logo': 'url(../assets/logo.png)',
+        'cloudyfi': 'url(../assets/cloudyfi.svg)',
+        'light-blue-shape': 'url(../assets/light-blue-shape.svg)',
+        'purple-shape': 'url(../assets/purple-shape.svg)',
+        'blue-shape': 'url(../assets/blue-shape.svg)',
       },
       boxShadow: {
-        'header': '0px 2px 4px rgba(148, 148, 148, 0.25)'
+        header: '0px 2px 4px rgba(148, 148, 148, 0.25)',
+        card: '0px 4px 4px rgba(0, 0, 0, 0.25)'
       },
       width: {
-        'logo': '70px'
+        logo: '70px',
+        cloudyfi: '364px'
       }
     },
   },

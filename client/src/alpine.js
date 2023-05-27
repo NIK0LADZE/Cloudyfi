e7a3f2cfed0b8f2b6d77bd0ebfa0d20d3daf7707
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2023-05-27 20:05:07 +0400

    Implement active menu animation

--- client/src/alpine.js
+++ client/src/alpine.js
@@ -10,6 +10,43 @@ Alpine.magic('scrollTo', (_, { evaluate }) => {
     };
 })
 
+const clearActiveMenuItems = (menuItems) => {
+    menuItems.forEach(menuItem => {
+        if (menuItem.classList.contains('active')) {
+            menuItem.classList.remove('active');
+        }
+    });
+}
+
+Alpine.magic('setActiveMenuItem', (_, { evaluate }) => {
+    return () => {
+        const aboutUs = evaluate('$refs.aboutUs').getBoundingClientRect();
+        const services = evaluate('$refs.services').getBoundingClientRect();
+        const packages = evaluate('$refs.packages').getBoundingClientRect();
+        const contact = evaluate('$refs.contact').getBoundingClientRect();
+        const aboutUsMenuItem = evaluate('$refs.aboutUsMenuItem');
+        const servicesMenuItem = evaluate('$refs.servicesMenuItem');
+        const packagesMenuItem = evaluate('$refs.packagesMenuItem');
+        const contactMenuItem = evaluate('$refs.contactMenuItem');
+
+        if (contact.top <= 90) {
+            clearActiveMenuItems([aboutUsMenuItem, servicesMenuItem, packagesMenuItem]);
+            contactMenuItem.classList.add('active');
+        } else if (packages.top <= 90) {
+            clearActiveMenuItems([aboutUsMenuItem, servicesMenuItem, contactMenuItem]);
+            packagesMenuItem.classList.add('active');
+        } else if (services.top <= 90) {
+            clearActiveMenuItems([aboutUsMenuItem, packagesMenuItem, contactMenuItem]);
+            servicesMenuItem.classList.add('active');
+        } else if (aboutUs.top <= 90) {
+            clearActiveMenuItems([servicesMenuItem, packagesMenuItem, contactMenuItem]);
+            aboutUsMenuItem.classList.add('active');
+        } else {
+            clearActiveMenuItems([aboutUsMenuItem, servicesMenuItem, packagesMenuItem, contactMenuItem]);
+        }
+    }
+})
+
 Alpine.magic('sendEmail', (formElement) => {
     return async () => {
         const formData = Object.fromEntries(new FormData(formElement));
